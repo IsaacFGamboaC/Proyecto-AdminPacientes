68a9c1270a06cf9b048913bb849deaa40e3d983b
Isaacfgc <isaacgamboacampos@gmail.com>
2023-04-26 10:41:04 -0600

    Fase 2 terminada v2.0

--- ProyectFnl/src/Formulario.jsx
+++ ProyectFnl/src/Formulario.jsx
@@ -26,18 +26,19 @@ export default function Formulario() {
     }
     return (
         <div>
+            <h1 className='titulos'>CREAR CITA</h1>
             <form className='inputForm' action="">
-                <label htmlFor="">Nombre Mascota</label>
-                <input onChange={(e) => { setMascota(e.target.value) }} value={mascota} type="text" />
-                <label htmlFor="">Nombre Dueño</label>
-                <input onChange={(e) => { setDueño(e.target.value) }} value={dueño} type="text" />
-                <label htmlFor="">Fecha</label>
-                <input onChange={(e) => { setFecha(e.target.value) }} value={fecha} type="date" />
-                <label htmlFor="">Hora</label>
-                <input onChange={(e) => { setHora(e.target.value) }} value={hora} type="time" />
-                <label htmlFor="">Sintomas</label>
-                <textarea onChange={(e) => { setSintomas(e.target.value) }} value={sintomas} name="Sintomas" cols="30" rows="10"></textarea>
-                <button onClick={agregarPaciente} type='button'>Agregar</button>
+                <label className='tit' htmlFor="">Nombre Mascota</label>
+                <input className='label' onChange={(e) => { setMascota(e.target.value) }} value={mascota} type="text" />
+                <label className='tit' htmlFor="">Nombre Dueño</label>
+                <input className='label' onChange={(e) => { setDueño(e.target.value) }} value={dueño} type="text" />
+                <label className='tit' htmlFor="">Fecha</label>
+                <input className='label' onChange={(e) => { setFecha(e.target.value) }} value={fecha} type="date" />
+                <label className='tit' htmlFor="">Hora</label>
+                <input className='label' onChange={(e) => { setHora(e.target.value) }} value={hora} type="time" />
+                <label className='tit' htmlFor="">Sintomas</label>
+                <textarea className='label' onChange={(e) => { setSintomas(e.target.value) }} value={sintomas} name="Sintomas" cols="30" rows="10"></textarea>
+                <button className='Submit' onClick={agregarPaciente} type='button'>Agregar</button>
             </form>
         </div>
     )
